eaca2e40588d6aad43aa816aa6e52bdd08e0351a
vihan13singh <desitonystark@gmail.com>
2021-06-30 15:48:09 +0530

    Add file handler, stream handler, and custom adapter to logger that removes old handlers on initialization

--- cl.py
+++ cl.py
@@ -2,17 +2,20 @@ import logging
 
 class CustomAdapter(logging.LoggerAdapter):
     def process(self, msg, kwargs):
-        print(self.extra)
         uuid = kwargs.pop('uuid', self.extra['uuid'])
-        focus = kwargs.pop('focus', self.extra['focus'])
-        return '[%s] [%s] %s' % (uuid, focus, msg), kwargs
+        f1 = str(kwargs.pop('f1', self.extra['f1']))
+        f2 = str(kwargs.pop('f2', self.extra['f2']))
+        f3 = str(kwargs.pop('f3', self.extra['f3']))
+        return 'uuid[%s] || f1[%s] f2[%s] f3[%s] || %s' % (uuid, f1, f2, f3, msg), kwargs
 
-def get_logger(uuid, level=logging.INFO):
+def get_logger(log_target, level=logging.INFO):
     logger = logging.getLogger(__name__)
+    for hdlr in logger.handlers[:]:  # remove all old handlers
+            logger.removeHandler(hdlr)
     logger.setLevel(level)
-    _log_format = f"%(asctime)s | [%(levelname)s] | %(name)s | (%(filename)s).%(funcName)s(%(lineno)d) | {uuid} | %(message)s"
+    _log_format = f"%(asctime)s || [%(levelname)s] || %(name)s || (%(filename)s).%(funcName)s(%(lineno)d) || {log_target} || %(message)s"
     # file handler
-    file_handler = logging.FileHandler(f"logs/{uuid}.log")
+    file_handler = logging.FileHandler(f"logs/{log_target}.log")
     file_handler.setLevel(level)
     file_handler.setFormatter(logging.Formatter(_log_format))
     logger.addHandler(file_handler)
@@ -21,21 +24,10 @@ def get_logger(uuid, level=logging.INFO):
     stream_handler.setLevel(level)
     stream_handler.setFormatter(logging.Formatter(_log_format))
     logger.addHandler(stream_handler)
+    # custom handler
+    logger = CustomAdapter(logger, {'uuid': None, 'f1': None, 'f2': None, 'f3': None})
     return logger
 
-def get_custom_logger():
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s",
-    )
-
-    logger = logging.getLogger(__name__)
-    logger = CustomAdapter(logger, {'uuid': None, 'focus': None})
-
-
 if __name__=='__main__':
-
-    logger.info('Both provided', uuid='123', focus='order')
-    logger.info('UUID provided', uuid='123')
-    logger.info('Neither provided')
-
+    logger = get_logger('jarvis')
+    logger.info('Order placed successfully', f1={'trade_id':'asfsadfasfsda'}, f2={'order_id':122})
